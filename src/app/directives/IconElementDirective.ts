8e8a7407d48f6590a4d2647d040a37f8cd8013d4
= <vcamposmiguel@gmail.com>
2026-07-05 18:05:09 -0500

    New elements design v1

--- src/app/directives/IconElementDirective.ts
+++ src/app/directives/IconElementDirective.ts
@@ -6,47 +6,49 @@ import { Directive, ElementRef, Input, OnInit, Renderer2  } from '@angular/core'
 })
 export class IconElementDirective implements OnInit{
   private readonly iconRoot = (icon: string) => {
-    return `<img src="./../../../assets/icons/${icon}.svg"/>`
+    return `<img src="./../../../assets/icons/${icon}.svg" class="icon-size"/>`
   }
   private readonly icons = new Map<string, string>([
-    ["Java", this.iconRoot('java')],
-    ["Spring", this.iconRoot('spring')],
-    ["JavaScript", this.iconRoot('javascript')],
-    ["React", this.iconRoot('react')],
-    ["Linkedin", this.iconRoot('linkedin')],
-    ["Github", this.iconRoot('github')],
-    ["CSharp", this.iconRoot('csharp')],
-    ["PHP", this.iconRoot('php')],
-    ["Mysql", this.iconRoot('mysql')],
-    ["SQLServer", this.iconRoot('sqlserver')],
-    ["VisualStudio", this.iconRoot('visualstudio')],
-    ["VisualStudioCode", this.iconRoot('visualstudiocode')],
-    ["Trello", this.iconRoot('trello')],
-    ["Angular", this.iconRoot('angular')],
-    ["AndroidStudio", this.iconRoot('androidstudio')]
+    ["Java", 'java'],
+    ["Spring", 'spring'],
+    ["JavaScript", 'javascript'],
+    ["React", 'react'],
+    ["Linkedin", 'linkedin'],
+    ["Github", 'github'],
+    ["CSharp", 'csharp'],
+    ["PHP", 'php'],
+    ["Mysql", 'mysql'],
+    ["SQLServer", 'sqlserver'],
+    ["VisualStudio", 'visualstudio'],
+    ["VisualStudioCode", 'visualstudiocode'],
+    ["Trello", 'trello'],
+    ["Angular", 'angular'],
+    ["AndroidStudio", 'androidstudio']
   ]);
 
   @Input('icon') iconKey!: string;
+  @Input('text') iconText!: string;
+  @Input('tooltip') iconTooltip!: string;
 
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
     const iconSvg = this.icons.get(this.iconKey);
+    
+    if (!iconSvg) return;
+    const img = this.renderer.createElement('img');
+    this.renderer.addClass(img, 'icon-size');
+    this.renderer.setAttribute(img, 'src', `./../../../assets/icons/${iconSvg}.svg`);
 
-    if (iconSvg) {
-      const span = this.renderer.createElement('span');
-      this.renderer.addClass(span, 'icon__stored');
-      this.renderer.addClass(span, 'bg-not');
-      this.renderer.addClass(span, 'vertical-flex');
-      
-      if (!this.el.nativeElement.getAttribute('title')) {
-        this.renderer.setAttribute(span, 'data-tooltip', this.iconKey);
-        this.renderer.addClass(span, 'tooltip');
-      }
-      
-      span.innerHTML = iconSvg;
-      this.renderer.insertBefore(this.el.nativeElement, span, this.el.nativeElement.firstChild);
-      
-    }
+    const span = this.el.nativeElement;
+    this.renderer.addClass(span, 'icon__stored');
+    this.renderer.addClass(span, 'bg-not');
+    this.renderer.addClass(span, 'vertical-flex');
+
+    this.renderer.setAttribute(span, 'data-tooltip', this.iconTooltip ? this.iconTooltip : this.iconKey );
+    this.renderer.addClass(span, 'tooltip');
+    
+    span.innerHTML = this.iconText ? this.iconRoot(iconSvg) + `<p class="icon-text">${this.iconText}</p>` : this.iconRoot(iconSvg);
+      //this.renderer.insertBefore(this.el.nativeElement, span, this.el.nativeElement.firstChild);
   }
 }
